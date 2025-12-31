80446ed84517ac028064f8f3129bbc5a91375574
Teqsys-erp <erp.support@teqsys.in>
2025-12-31 15:41:59 +0530

    Rename app from 'Frappe HR' to 'Citta HR'

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,8 +23,8 @@ export default defineConfig({
 			},
 			manifest: {
 				display: "standalone",
-				name: "Frappe HR",
-				short_name: "Frappe HR",
+				name: "Citta HR",
+				short_name: "Citta HR",
 				start_url: "/hrms",
 				description: "Everyday HR & Payroll operations at your fingertips",
 				theme_color: "#ffffff",
