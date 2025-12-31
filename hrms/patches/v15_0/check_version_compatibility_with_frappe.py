a25ec6ea3d662b895c075d62cc1d021bd7d90b4f
Teqsys-erp <erp.support@teqsys.in>
2025-12-31 16:10:24 +0530

    Update check_version_compatibility_with_frappe.py

--- hrms/patches/v15_0/check_version_compatibility_with_frappe.py
+++ hrms/patches/v15_0/check_version_compatibility_with_frappe.py
@@ -11,7 +11,7 @@ def execute():
 
 	if frappe_v.startswith("14") and hrms_v.startswith("15"):
 		message = f"""
-			The `develop` branch of Frappe HR is no longer compatible with Frappe & ERPNext's `version-14`.
+			The `develop` branch of Citta HR is no longer compatible with Frappe & ERPNext's `version-14`.
 			Since you are using ERPNext/Frappe `version-14` please switch Frappe HR's branch to `version-14` and then proceed with the update.\n\t
 			You can switch the branch by following the steps mentioned here: {WIKI_URL}
 		"""
